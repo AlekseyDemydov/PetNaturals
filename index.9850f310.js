cc04aad92633c467f1f2abc767bdf1f365ae868a
AlekseyDemydov <96746949+AlekseyDemydov@users.noreply.github.com>
2024-09-15 15:35:33 +0000

    Deploying to gh-pages from @ AlekseyDemydov/PetNaturals@1e3f4d0e490ac29d7e26f448e5c822ccdda4bbc7 🚀

---
+++ index.9850f310.js
@@ -0,0 +1,2 @@
+var burgerMenu=document.getElementById("burger-menu"),navigation=document.getElementById("navigation");burgerMenu.addEventListener("click",(function(){navigation.classList.toggle("active")}));
+//# sourceMappingURL=index.9850f310.js.map
